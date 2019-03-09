d269e27fb4f04c31b66caa15a9dcc7594134e5d0
knuxfanwin8 <knuxfanwin8.mail@gmail.com>
2019-03-09 23:43:10 +0100

    rebrand to lineage, fix board config for cm14 (currently pushed to the wrong branch, will fix later) (requires moving COMMON_GLOBAL_C* to build/core/config.mk around line 676

---
+++ lineage.mk
@@ -0,0 +1,16 @@
+# Release name
+PRODUCT_RELEASE_NAME := GalaxyGrandNeo
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/baffinlite/lineage_baffinlite.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := baffinlite
+PRODUCT_NAME := lineage_baffinlite
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-I9060
+
