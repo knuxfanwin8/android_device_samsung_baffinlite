1fbe63b76aac593dae818caf6019954c67e33558
knuxify <knuxfanwin8.mail@gmail.com>
2019-06-04 16:24:42 +0200

    fix broken paths

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := GalaxyGrandNeo
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/baffinlite/lineage_baffinlite.mk)
